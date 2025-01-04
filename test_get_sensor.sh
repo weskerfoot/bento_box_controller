be6825787d06e262024c353b35b884d3019edaf2
Wesley Kerfoot <wjak56@gmail.com>
2025-01-04 17:35:20 -0500

    add test scripts

---
+++ test_get_sensor.sh
@@ -0,0 +1,2 @@
+#! /usr/bin/env bash
+curl -XGET http://192.168.0.41/sensor
--- test_set_thresholds.sh
+++ test_set_thresholds.sh
@@ -1,2 +1,3 @@
 #! /usr/bin/env bash
-curl -XPOST http://192.168.0.41/sensor -d '{"voc_max_threshold": 120}'
+#curl -XPOST http://192.168.0.41/sensor -d '{"voc_max_threshold": 150, "voc_min_threshold" : 140}'
+curl -XPOST http://192.168.0.41/sensor -d '{"voc_max_threshold": 130, "voc_min_threshold" : 120}'
